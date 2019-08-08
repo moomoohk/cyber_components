645a1afdd32d5a1b912ddab74c4df6a472cf35c2
Meshulam Silk <moomoohk@ymail.com>
2019-08-09 00:06:37 +0300

    Auto add objects to DB

--- cyber_components/db/__init__.py
+++ cyber_components/db/__init__.py
@@ -1,4 +1,8 @@
+from sqlalchemy import event
+from sqlalchemy.orm import mapper
+
 from cyber_components.db.connection import Base, session, engine
+from cyber_components.db.connection import Session as _Session
 
 from cyber_components.db.associations.dns_suffixes import DnsSuffixes
 from cyber_components.db.associations.dns_servers import DnsServers
@@ -16,12 +20,10 @@ from cyber_components.db.models.os_info import OsInfo
 from cyber_components.db.models.hardware_info import HardwareInfo
 from cyber_components.db.models.regional_info import RegionalInfo
 
-
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 
-def get_interface_by_ip(ip: str):
-    return session.query(NetworkInterface)\
-        .filter(NetworkInterface.ipv4 == ip)\
-        .one_or_none()
+@event.listens_for(mapper, "init")
+def auto_add(target, args, kwargs):
+    session.add(target)
